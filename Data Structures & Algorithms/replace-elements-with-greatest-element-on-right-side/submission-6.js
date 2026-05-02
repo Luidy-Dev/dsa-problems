66517a2519cee9d14b155faa2948ef31df9c80d4
luidy <luidyh027@gmail.com>
2026-05-02 15:49:46 -0300

    Add: replace-elements-with-greatest-element-on-right-side - submission-6

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-6.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+    
+      let ans = new Array(arr.lenght)
+
+      let maiorElemento = -1
+    //interação reversa pra evitar escaneamento desnecessário 
+      for ( i = arr.lenght; i >= 0; i--){
+        arr[1] = maiorElemento
+      }
+    }
+}
